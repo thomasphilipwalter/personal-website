dd2642e053903e3f440517cff9e7c4aebe98f0ef
Thomas Walter <thomasphilipwalter@gmail.com>
2025-03-19 11:30:26 -0700

    fix link

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -2,11 +2,10 @@ function Resources() {
   return (
     <nav className="bg-white text-blue-700 py-4 w-full shadow-md">
       <ul className="flex justify-center space-x-6">
-        {/* Resume - Download */}
         <li>
           <a
-            href="/static/thomas_walter_resume.pdf" // Adjust path based on file location
-            download
+            href="/thomas_walter_resume.pdf" 
+            target="_blank"
             className="hover:text-gray-300"
           >
             Resume
