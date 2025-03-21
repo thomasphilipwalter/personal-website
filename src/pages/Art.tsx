e2591a2192b0995a62179518593cb0d2a33e384a
Thomas Walter <thomasphilipwalter@gmail.com>
2025-03-20 18:02:36 -0700

    update resume, bg colors

--- src/pages/Art.tsx
+++ src/pages/Art.tsx
@@ -1,6 +1,6 @@
 function Art() {
     return (
-      <div className="flex items-center justify-center h-screen bg-gray-400">
+      <div className="flex items-center justify-center h-screen bg-white">
         <h2 className="text-3xl font-bold">Art</h2>
       </div>
     )
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,6 +1,6 @@
 function Music() {
     return (
-      <div className="flex items-center justify-center h-screen bg-gray-300">
+      <div className="flex items-center justify-center h-screen bg-white">
         <h2 className="text-3xl font-bold">Music</h2>
       </div>
     )
